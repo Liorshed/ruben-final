0a2dde1c7b381089320011f95166da41f80bd1bc
Sheffy <ccdobu@gmail.com>
2025-01-06 15:09:16 +0200

    657

---
+++ script.js
@@ -0,0 +1,41 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+    
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+  const openingButton = document.getElementById('collapsiblebutton2')
+  const box = document.getElementsByClassName('content')
+  openingButton.addEventListener('click', () => {
+box.classList.content('opened');
+  if (box.classList.contains('opened')){
+    openingButton.style.color = 'red'
+  } else {
+    openingButton.style.color = 'black'
+  } 
+})
