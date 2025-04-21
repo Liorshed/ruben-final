391d4fabbc9862a25d65ae67f790d79ad04dfa86
Sheffy <ccdobu@gmail.com>
2025-04-21 16:50:56 +0300

    4

--- script.js
+++ script.js
@@ -204,3 +204,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
